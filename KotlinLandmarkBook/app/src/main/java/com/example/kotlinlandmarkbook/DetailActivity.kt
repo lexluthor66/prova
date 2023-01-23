370fed5e67142181bd6a85042360f056bc4021e2
lexluthor66 <alessandro.bravetti@tiscali.it>
2023-01-23 16:35:08 +0100

    COMMIT OK

--- KotlinLandmarkBook/app/src/main/java/com/example/kotlinlandmarkbook/DetailActivity.kt
+++ KotlinLandmarkBook/app/src/main/java/com/example/kotlinlandmarkbook/DetailActivity.kt
@@ -2,10 +2,26 @@ package com.example.kotlinlandmarkbook
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import com.example.kotlinlandmarkbook.databinding.ActivityDetailBinding
+import com.example.kotlinlandmarkbook.databinding.ActivityMainBinding
 
 class DetailActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDetailBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail)
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val intent = intent
+        val name = intent.getStringExtra("landmark")
+        var  textView = binding.textView
+        textView.text = name
+        var  imageView = binding.imageView
+
+        val image = Globals.Chosen.returnImage()
+        imageView.setImageBitmap(image)
+
     }
 }
--- MySimpleAdapter/app/src/main/java/com/example/mysimpleadapter/CustomSimpleAdapter.kt
+++ MySimpleAdapter/app/src/main/java/com/example/mysimpleadapter/CustomSimpleAdapter.kt
@@ -1,4 +1,52 @@
 package com.example.mysimpleadapter
+import android.content.Context
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.SimpleAdapter
+import android.widget.TextView
+import android.widget.Toast
+import androidx.annotation.IdRes
+import androidx.annotation.LayoutRes
 
-class CustomSimpleAdapter {
+class CustomSimpleAdapter(
+    private val mContext: Context,
+    data: MutableList<HashMap<String, String>>,
+    @LayoutRes
+    res: Int,
+    from: Array<String>,
+    @IdRes
+    to: IntArray
+) :
+// Passing these params to SimpleAdapter
+    SimpleAdapter(mContext, data, res, from, to) {
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
+
+        // Get the view in our case list_item.xml
+        val view = super.getView(position, convertView, parent)
+
+        // Getting reference of ImageView that we
+        // have used in our list_item.xml file
+        // so that we can add user defined code
+        val avatarImageView = view.findViewById<ImageView>(R.id.avatarImageView)
+
+        // Reference of TextView which is treated a title
+        val titleTextView = view.findViewById<TextView>(R.id.titleTextView)
+
+        // Adding an clickEvent to the ImageView, as soon as we click this
+        // ImageView we will see a Toast which will display a message
+        // Note: this event will only fire when ImageView is pressed and
+        //	 not when whole list_item is pressed
+        avatarImageView.setOnClickListener {
+            Toast.makeText(
+                mContext,
+                "Image with title ${titleTextView.text} is pressed",
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+
+        // Finally returning our view
+        return view
+    }
 }
